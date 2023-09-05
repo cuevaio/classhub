8546105d19a840589c0f262d8b6dbe001991ae05
Anthony Cueva <hi.cuevantn@gmail.com>
2023-09-05 10:19:51 -0500

    shorcut for logout + ui

--- src/components/navbar/actions.tsx
+++ src/components/navbar/actions.tsx
@@ -2,8 +2,8 @@ import { Button } from "../ui/button";
 import { ProfileActions } from "./profile-actions";
 import Link from "next/link";
 const NavbarActions = () => (
-  <div className="flex space-x-2 justify-end">
-    <Button asChild>
+  <div className="flex space-x-2 justify-end items-center">
+    <Button asChild size="sm">
       <Link href="/app/create">Publicar</Link>
     </Button>
     <ProfileActions />
--- src/components/navbar/profile-actions.tsx
+++ src/components/navbar/profile-actions.tsx
@@ -14,18 +14,35 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import * as React from "react";
+
+import { signOut } from "next-auth/react";
 
 const ProfileActions = () => {
   let { isLoading, profile } = useCurrentUser();
+
+  React.useEffect(() => {
+    const logoutShorcut = (e: KeyboardEvent) => {
+      if (e.key === "Q" && (e.metaKey || e.ctrlKey) && e.shiftKey) {
+        e.preventDefault();
+        signOut({ callbackUrl: "/" });
+      }
+    };
+
+    document.addEventListener("keydown", logoutShorcut);
+    return () => document.removeEventListener("keydown", logoutShorcut);
+  }, []);
+
   if (isLoading || !profile?.name)
-    return <Skeleton className="w-10 h-10 rounded-full" />;
+    return <Skeleton className="w-12 h-12 rounded-full" />;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-10 w-10 rounded-full">
-          <Avatar className="h-10 w-10">
+        <Button variant="ghost" className="relative h-12 w-12 rounded-full">
+          <Avatar className="h-12 w-12">
             <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>{profile.name?.slice(0, 1)}</AvatarFallback>
+            <AvatarFallback>{profile.name.slice(0, 1)}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
@@ -33,7 +50,7 @@ const ProfileActions = () => {
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1.5">
             <p className="text-sm leading-none">{profile.name}</p>
-            <p className="text-sm font-medium leading-none">
+            <p className="text-xs leading-none text-muted-foreground">
               @{profile.handle}
             </p>
             <p className="text-xs leading-none text-muted-foreground">
@@ -58,7 +75,7 @@ const ProfileActions = () => {
           <DropdownMenuItem>New Team</DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={() => signOut({ callbackUrl: "/" })}>
           Log out
           <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
         </DropdownMenuItem>
